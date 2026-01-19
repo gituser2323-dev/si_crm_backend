505ce487d0a5712f8546eb9b93465569a64d94f0
gituser2323-dev <gituser2323@gmail.com>
2026-01-19 10:42:10 +0530

    file changes for vercel

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cors({
     credentials:true
 }))
 
-app.get('/',(req,res)=>{
+app.get('/api',(req,res)=>{
     res.status(200).json({
         message:"Server Is up...",
         success:true
